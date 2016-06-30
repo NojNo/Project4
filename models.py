4e0bf8c5d6a7e6298bfc4211dad99b183bac2770
NojNo <nojan@hotmail.de>
2016-06-30 21:28:51 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,22 +5,23 @@ classes they can include methods."""
 import random
 from protorpc import messages
 from google.appengine.ext import ndb
+from datetime import date
+
 
 class Player(ndb.Model):
-    """User profile"""
+    """Players profile"""
     name = ndb.StringProperty(required=True)
-    email =ndb.StringProperty()
+    email = ndb.StringProperty()
+
 
 # what does stringmessage do
 # if I want to display a message like 'User {} created!' I need this class
+# stringMessage is amazing during testing!!!!!
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
     message = messages.StringField(1, required=True)
 
-class possible_wins(ndb.Model):
-    win1 = ndb.IntegerProperty(repeated=True)
 
-# whats the score is missing. I need end of game
 class Game(ndb.Model):
     """Game object"""
     player1 = ndb.KeyProperty(required=True, kind='Player')
@@ -39,17 +40,21 @@ class Game(ndb.Model):
                     player2=player2,
                     winner1=False,
                     winner2=False,
-                    available_positions=[1,2,3,4,5,6,7,8,9],
+                    available_positions=[1, 2, 3, 4, 5, 6, 7, 8, 9],
                     player1_position=[],
                     player2_position=[],
                     moves=[])
         game.put()
         return game
 
-    def to_form2(self):
-        """Returns a GameForm representation of the Game"""
+    def to_form_without_message(self):
+        """Returns a GameForm representation of the Game without
+        a message and the urlsafe_key
+        """
         form = GameForm2()
-        form.player1 = self.player1.get().name # why user.get().name because user is object. we need the name attribute of this object
+        form.player1 = self.player1.get().name
+        # why user.get().name because user is object.
+        # we need the name attribute of this object
         form.player2 = self.player2.get().name
         form.winner1 = self.winner1
         form.winner2 = self.winner2
@@ -62,53 +67,62 @@ class Game(ndb.Model):
     def to_form(self, message):
         """Returns a GameForm representation of the Game"""
         form = GameForm()
-        form.urlsafe_key = self.key.urlsafe() # auto -generated
-        form.player1 = self.player1.get().name # why user.get().name because user is object. we need the name attribute of this object
+        form.urlsafe_key = self.key.urlsafe()
+        # auto -generated
+        form.player1 = self.player1.get().name
+        # why user.get().name because user is object. we need the
+        # name attribute of this object
         form.player2 = self.player2.get().name
         form.winner1 = self.winner1
         form.winner2 = self.winner2
-        form.message = message # message is the input of the function and is set in tictactoe.py
+        form.message = message
+        # message is the input of the function and is set in tictactoe.py
         form.available_positions = self.available_positions
         form.player1_position = self.player1_position
         form.player2_position = self.player2_position
         form.moves = self.moves
         return form
 
-    def end_of_game1(self, won=False):
-        self.winner1 = True
-        self.put()
-        score = Score(player=self.player1.get().name, date=str(self.date))
-        score.put()
+    def end_of_game(self, player1_win=False, player2_win=False):
+        """If there is a winner, end_of_game changes a property to true and
+        saves the score in a Score"""  
+        self.winner1 = player1_win
+        self.winner2 = player2_win
+        if player1_win:  # ==True
+            score = Score(player=self.player1, date=date.today())
+            score.put()
+        elif player2_win:  # ==True
+            score = Score(player=self.player2, date=date.today())
+            score.put()
+        else:
+            pass
 
 
-    def end_of_game2(self, won=False):
-        self.winner2 = True
-        self.put()
-        """score = Score(player=self.player2.get().name, won=self.won,
-                      date=str(self.date))
-        score.put()"""
-
+# relevant infos for a new game
 class NewGameForm(messages.Message):
     """Used to create a new game"""
     player1 = messages.StringField(1, required=True)
     player2 = messages.StringField(2, required=True)
 
+
 class GameForm(messages.Message):
-    """GameForm for outbound game state information"""
+    """GameForm for outbound details on the game"""
     player1 = messages.StringField(1, required=True)
     player2 = messages.StringField(2, required=True)
     winner1 = messages.BooleanField(3, required=True)
     winner2 = messages.BooleanField(4, required=True)
     urlsafe_key = messages.StringField(5, required=True)
     message = messages.StringField(6, required=True)
-    available_positions = messages.IntegerField(7, repeated = True)
-    player1_position = messages.IntegerField(8, repeated = True)
-    player2_position = messages.IntegerField(9, repeated = True)
-    moves = messages.IntegerField(10, repeated = True)
+    available_positions = messages.IntegerField(7, repeated=True)
+    player1_position = messages.IntegerField(8, repeated=True)
+    player2_position = messages.IntegerField(9, repeated=True)
+    moves = messages.IntegerField(10, repeated=True)
+
 
 class MakeMoveForm(messages.Message):
     """Used to make a move in an existing game"""
     position = messages.IntegerField(1, required=True)
+
 
 class GameForm2(messages.Message):
     """GameForm for outbound game state information"""
@@ -116,14 +130,15 @@ class GameForm2(messages.Message):
     player2 = messages.StringField(2, required=True)
     winner1 = messages.BooleanField(3, required=True)
     winner2 = messages.BooleanField(4, required=True)
-    available_positions = messages.IntegerField(5, repeated = True)
-    player1_position = messages.IntegerField(6, repeated = True)
-    player2_position = messages.IntegerField(7, repeated = True)
-    moves = messages.IntegerField(8, repeated = True)
+    available_positions = messages.IntegerField(5, repeated=True)
+    player1_position = messages.IntegerField(6, repeated=True)
+    player2_position = messages.IntegerField(7, repeated=True)
+    moves = messages.IntegerField(8, repeated=True)
+
 
 class GameForms(messages.Message):
     """Return multiple GameForms"""
-    items = messages.MessageField(GameForm2, 1, repeated = True)
+    items = messages.MessageField(GameForm2, 1, repeated=True)
 
 
 class Score(ndb.Model):
@@ -132,15 +147,43 @@ class Score(ndb.Model):
     date = ndb.DateProperty(required=True)
 
     def to_form(self):
-        return ScoreForm(player=self.user.get().name, date=str(self.date))
+            return ScoreForm(player=self.player.get().name,
+                             date=str(self.date))
+
 
 class ScoreForm(messages.Message):
     """ScoreForm for outbound Score information"""
     player = messages.StringField(1, required=True)
     date = messages.StringField(2, required=True)
 
-class ScoreForms(messages.Message):
-    """Return multiple ScoreForms"""
-    items = messages.MessageField(ScoreForm, 1, repeated=True)
-   # player2_position = messages.MessageField(2, repeated=True)
-   # moves = messages.MessageField(3, repeated=True)
+
+class Ranking(ndb.Model):
+    """Ranking object"""
+    player = ndb.StringProperty(required=True)
+    number_of_wins = ndb.IntegerProperty(required=True)
+
+    @classmethod
+    def new_in_ranking(cls, player, number_of_wins):
+        """Creates and returns a user in a ranking"""
+        new_in_ranking = Ranking(player=player,
+                                 number_of_wins=number_of_wins)
+        new_in_ranking.put()
+        return new_in_ranking
+
+    def to_form(self):
+        """Returns a RankingForm representation of the Game without"""
+        form = RankingForm()
+        form.player = self.player
+        form.number_of_wins = self.number_of_wins
+        return form
+
+
+class RankingForm(messages.Message):
+    """RankingForm for outbound Ranking information"""
+    player = messages.StringField(1, required=True)
+    number_of_wins = messages.IntegerField(2, required=True)
+
+
+class RankingForms(messages.Message):
+    """Return multiple Rankingforms"""
+    items = messages.MessageField(RankingForm, 1, repeated=True)
